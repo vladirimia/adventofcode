29ffad996630086d512d8dcf0217ba23c00b651d
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-12-06 17:32:50 +0200

    start day06

--- src/Aoc2023/Day05.cs
+++ src/Aoc2023/Day05.cs
@@ -1,5 +1,14 @@
-﻿namespace Aoc2023;
+﻿using Aoc2023.Helpers;
 
-internal class Day05
+namespace Aoc2023;
+
+public class Day05 : ISolver
 {
+    public string DayName => nameof(Day06);
+    private string[] lines;
+
+    public void Solve()
+    {
+        lines = ReadWriteHelpers.ReadTextFile(DayName);
+    }
 }
