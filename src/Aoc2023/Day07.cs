fa779075ff05dd56480f14e38a47418adbca34d6
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-12-07 11:37:07 +0200

    start day07

--- src/Aoc2023/Day07.cs
+++ src/Aoc2023/Day07.cs
@@ -1,5 +1,48 @@
-﻿namespace Aoc2023;
+﻿using Aoc2023.Helpers;
 
-internal class Day07
+namespace Aoc2023;
+
+public class Day07 : ISolver
 {
+    public string DayName => nameof(Day07);
+    private string[] lines;
+
+    public void Solve()
+    {
+        lines = ReadWriteHelpers.ReadTextFile(DayName);
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ');
+        }
+    }
 }
+
+record PokerCard(string Value)
+{
+    public int Strength
+    {
+        get
+        {
+            return Value switch
+            {
+                "A" => 15,
+                "K" => 14,
+                "Q" => 13,
+                "J" => 12,
+                "T" => 10,
+                "9" => 9,
+                "8" => 8,
+                "7" => 7,
+                "6" => 6,
+                "5" => 5,
+                "4" => 4,
+                "3" => 3,
+                "2" => 2,
+                _ => 0
+            };
+        }
+    }
+}
+
+record PokerHand(List<PokerCard> Cards, int Bet);
